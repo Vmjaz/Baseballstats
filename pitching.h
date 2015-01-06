e098561be81b60ba204a27ce54eeb194fb041849
Vmjaz <jaquezv97@gmail.com>
2015-01-06 18:12:14 -0500

    Create pitching.h

---
+++ pitching.h
@@ -0,0 +1,9 @@
+#ifndef PITCHING_H_INCLUDED
+#define PITCHING_H_INCLUDED
+class pitching {
+	double games_started, wins, losses, decisions, winningpercentage, no_decisions;
+	public:
+		pitching();
+		void calculate();
+};
+#endif // PITCHING_H_INCLUDED
