1dc8ed1d29ba42b0a14e896f1b343571399182ce
Vmjaz <jaquezv97@gmail.com>
2015-01-06 18:12:31 -0500

    Create hitting.h

---
+++ hitting.h
@@ -0,0 +1,11 @@
+#ifndef HITTING_H_INCLUDED
+#define HITTING_H_INCLUDED
+
+class hitting {
+	double plateappearance, hits, battedout, at_bats, battingavg, walk;
+	public:
+		hitting();
+		void calculate();
+};
+
+#endif // HITTING_H_INCLUDED
