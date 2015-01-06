13185ea054469733d12b97f2a5534f43972efbe8
Vmjaz <jaquezv97@gmail.com>
2015-01-06 18:13:04 -0500

    Create fielding.h

---
+++ fielding.h
@@ -0,0 +1,9 @@
+#ifndef FIELDING_H_INCLUDED
+#define FIELDING_H_INCLUDED
+class fielding {
+	double chances, put_outs, assists, attempts, fieldingpercent, errors;
+	public:
+		fielding();
+		void calculate();
+};
+#endif // FIELDING_H_INCLUDED
